4bb97041311a5597875caa47e850b9b738f5028d
moo <MooDeveloper@gmail.com>
2017-02-01 12:28:32 -0500

    Heading Jquery almost done

--- index.js
+++ index.js
@@ -6,3 +6,13 @@ $('.navbar-nav').on('click', 'a', function (e) {
     $('body').scrollTo($($(this).attr('href')).position());
   }
 });
+
+$(document).scroll(function () {
+  $('.display-3').each(function() {
+    if((document.documentElement.scrollTop || document.body.scrollTop > $(this).next().position().top-100) && (document.documentElement.scrollTop || document.body.scrollTop < $(this).next().position().top+$(this).next().height())){
+      $(this).css({position: 'fixed', 'top': '1em', 'margin-top': '-8px'});
+    } else {
+      $(this).removeAttr("style");
+    }
+  })
+});
