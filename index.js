0350bbc9d2f162e578993433515bd1bf000a9096
moo <MooDeveloper@gmail.com>
2017-02-01 19:52:16 -0500

    Working Watermarking

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
-$('.navbar-nav').on('click', 'a', function (e) {
-  e.preventDefault();
-  if ($(this).attr('href') === '#Home') {
-    $('body').scrollTo('top');
-  } else {
-    $('body').scrollTo($($(this).attr('href')).position());
-  }
+$('.navbar-nav').on('click', 'a', function(e) {
+    e.preventDefault();
+    if ($(this).attr('href') === '#Home') {
+        $('body').scrollTo('top');
+    } else {
+        $('body').scrollTo($($(this).attr('href')).position());
+    }
 });
 
-$(document).scroll(function () {
-  $('.display-3').each(function() {
-    if((document.documentElement.scrollTop || document.body.scrollTop > $(this).next().position().top-100) && (document.documentElement.scrollTop || document.body.scrollTop < $(this).next().position().top+$(this).next().height())){
-      $(this).css({position: 'fixed', 'top': '1em', 'margin-top': '-8px'});
-    } else {
-      $(this).removeAttr("style");
-    }
-  })
+$(document).scroll(function() {
+    $('.section').each(function() {
+        if ((document.documentElement.scrollTop || document.body.scrollTop > $(this).next().position().top - 950) && (document.documentElement.scrollTop || document.body.scrollTop < $(this).next().position().top + $(this).next().height() - 950)) {
+            if ((document.documentElement.scrollTop || document.body.scrollTop > $(this).next().children().first().next().position().top - 950) && (document.documentElement.scrollTop || document.body.scrollTop < $(this).next().children().first().next().position().top + $(this).next().children().first().next().height() - 950)) {
+                $(this).hide();
+            } else {
+                $(this).show();
+            }
+        } else {
+            $(this).hide();
+        }
+    })
 });
